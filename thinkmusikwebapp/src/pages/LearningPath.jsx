ca62ce2920c1422970e4f37a671caa7419e7f186
Onimcodes <Abrahammicheal55@yahoo.com>
2023-08-17 11:03:49 +0100

    little changes

--- thinkmusikwebapp/src/pages/LearningPath.jsx
+++ thinkmusikwebapp/src/pages/LearningPath.jsx
@@ -1,8 +1,27 @@
 import React from 'react'
-
+import './'
+import badgeImage from '../assets/badge.png'
 const LearningPath = () => {
   return (
-    <div>LearningPath</div>
+    <div className='overview__container'>
+        <div className="learningpath__header">
+        <a className="learningpath__header-link">
+            Home   /   Learning Path
+        </a>
+        </div>
+       <div className="learningpath__progress-div">
+       <div className="learningpath__badge-img">
+            <img src={badgeImage} alt="" />
+        </div>
+        <p>Beginner</p>
+        <div className="learningpath__progressbar">
+            <div className="learningpath__progress">
+
+            </div>
+        </div>
+        <p>Advance</p>
+       </div>
+    </div>
   )
 }
 
